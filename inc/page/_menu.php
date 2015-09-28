a18ea871f9a54d08d07f351f658792078d953753
Stand Lee <michungwei@gmail.com>
2015-09-28 20:20:59 +0800

    點選帳號下拉選單實作

--- inc/page/_menu.php
+++ inc/page/_menu.php
@@ -1,3 +1,34 @@
+<script>
+$(document).ready(function(e) {
+    demo();
+    $(".memberSelect").click(function(){
+        if($(".memberSelectItem").css("display") == "none")
+        {
+            $(".memberSelectItem").css("display","block");
+            $(".memberSelectItem").css("left",$(".m_name").position().left + 13);
+            //console.log($(".m_name").position().left);
+        }
+        else
+            $(".memberSelectItem").css("display","none");
+    });
+    $(".memberMenu").find(".memberSelectItem>li").click(function() {
+        var isVal = $(this).text();
+        //$(".numSelect").find(".beSelect").text(isVal);
+        var order = $(this).attr("href");
+        $(location).attr('href', order);
+        //console.log(order);
+    });
+});
+
+</script>
+<div class="memberMenu">
+    <ul class="memberSelectItem">
+        <li href="memberCenter_<?php echo $_SESSION["session_id"]; ?>.html"> 會員中心 </li>
+        <li href="wishlist.html"> wishlist </li>
+        <li href="wishlist.html"> 訂單查詢 </li>
+        <li href="wishlist.html"> 購物抵用金查詢 </li>
+    </ul>
+</div>
 <div id="nav">
     <a href="index.html"><h1 class="logo"></h1></a>
     <ul class="menu">
@@ -6,13 +37,17 @@
         <li class="menu-item menu-access"><a href="product-list_2.html">accessories</a></li>
         <li class="menu-item menu-other"><a href="product-list_3.html">others</a></li>
         <li class="menu-item menu-look"><a href="lookbook.html">lookbook</a></li>
-        <li class="menu-item menu-wish"><a href="wishlist.html">wishlist</a></li>
+        <!--<li class="menu-item menu-wish"><a href="wishlist.html">wishlist</a></li>-->
     </ul>
     <ul class="submenu">
     	<?php
 			if(isLogin() || chkCookie()){
 		?>
-        	<li class="sub-item m_name"><a href="memberCenter_<?php echo $_SESSION["session_id"]; ?>.html"><?php echo $_SESSION["session_acc"]; ?></a></li>
+        	<li class="sub-item m_name">
+                <div class="memberSelect"> <span class="beSelect"> <?php echo $_SESSION["session_acc"]; ?> </span>
+                </div>
+                <!--<a href="memberCenter_<?php echo $_SESSION["session_id"]; ?>.html"><?php echo $_SESSION["session_acc"]; ?></a>-->
+            </li>
         	<li class="sub-item" style="font-weight:900; font-size:12px; margin-right:20px; border-right-color:#000000; border-right-style:solid; border-right-width:2px; padding-right:10px; height:15px;"><a href="javascript:;" class="log_out" onclick="logOut()">logout</a></li>
         <?php
 			}else{
